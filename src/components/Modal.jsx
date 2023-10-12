c7b19aa93a587b754ac2136ef16a45b54ec24ba4
lukexgraham <lukexxgraham@gmail.com>
2023-10-12 01:53:38 +0100

    added modals for cards

---
+++ src/components/Modal.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+const getAssetSrc = (name) => {
+    if (!name) {
+        return;
+    }
+    const path = `/src/assets/images/${name}`;
+    const modules = import.meta.glob("/src/assets/images/*", { eager: true });
+    return modules[path].default;
+};
+
+const Modal = ({ handleClose, show, project }) => {
+    const showHideClassName = show ? "display-block" : "display-none";
+    return (
+        <div id="modal" className={showHideClassName}>
+            <div className="modal-main">
+                <button className="modal-close" onClick={handleClose}>
+                    <AiOutlineClose />
+                </button>
+                <div className="modal-top">
+                    <div className="modal-image">
+                        <img
+                            src={getAssetSrc(project.img)}
+                            alt={project.title}
+                        />
+                    </div>
+                </div>
+                <div className="modal-bottom">
+                    <p className="project-title">{project.title}</p>
+                    <p className="project-subheading">{project.tools}</p>
+                    <ul>
+                        {project.desc.map((desc) => (
+                            <li>{desc}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 
-const ProjectCard = ({ project }) => {
+const getAssetSrc = (name) => {
+    if (!name) {
+        return;
+    }
+    const path = `/src/assets/images/${name}`;
+    const modules = import.meta.glob("/src/assets/images/*", { eager: true });
+    return modules[path].default;
+};
+
+const ProjectCard = ({ project, toggleModal }) => {
     return (
         <div className="card-container">
-            <div className="project">
-                {/*const ok = project.desc.map((bullet) => <li>{bullet}</li>);*/}
+            <div
+                className="project"
+                onClick={() => {
+                    toggleModal(project);
+                    document.body.style.overflow = "hidden";
+                }}
+            >
                 <div className="image">
                     <img
-                        src={"/" + project.img}
+                        src={getAssetSrc(project.img)}
                         alt={project.title}
-                        srcset=""
+                        srcSet=""
                     />
                 </div>
                 <div className="image-overlay">
